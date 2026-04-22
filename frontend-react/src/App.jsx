173f50047ac40452929f53790c9cf2d3902d56a5
Sunil002-git <sunilmarri886@gmail.com>
2026-04-22 12:09:13 +0530

    ML Predictions

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './assets/css/style.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Register from './components/Register'
+import Login from './components/Login'
+import AuthProvider from './AuthProvider'
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+  <AuthProvider>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+          <Route path='/' element={<Main />}/>
+          <Route path='/register' element={<PublicRoute><Register /></PublicRoute>} />
+          <Route path='/login' element={<PublicRoute><Login /></PublicRoute>} />
+          <Route path='/dashboard' element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+      </Routes>
+    <Footer />
+    </BrowserRouter>   
+  </AuthProvider>
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontend-react/src/AuthProvider.jsx
@@ -0,0 +1,19 @@
+import {useState, useContext, createContext} from 'react'
+
+// Create the COntext
+const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(
+        !!localStorage.getItem('access_token')
+    )
+  return (
+    <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
+export {AuthContext};
