503914ce54fdb3a99c271c9885bc94fe87bb3e04
Sunil002-git <sunilmarri886@gmail.com>
2026-04-24 11:55:37 +0530

    clean project without env

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,7 +1,19 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import axiosInstance from '../../axiosInstance'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 const Dashboard = () => {
+    const [ticker, setTicker] = useState('')
+    const [error, setError] = useState()
+    const [loading, setLoading] = useState(false)
+    const [plot, setPlot] = useState()
+    const [ma100, setMA100] = useState()
+    const [ma200, setMA200] = useState()
+    const [prediction, setPrediction] = useState()
+    const [mse, setMSE] = useState()
+    const [rmse, setRMSE] = useState()
+    const [r2, setR2] = useState()
     useEffect(() => {
         const fetchProtectedData = async () =>{
             try{
@@ -19,8 +31,83 @@ const Dashboard = () => {
         }
         fetchProtectedData();
     }, [])
+
+    const handleSubmit  = async (e) => {
+        e.preventDefault();
+        setLoading(true)
+        try{
+            const response = await axiosInstance.post('/predict/', {
+                ticker: ticker
+            });
+            console.log(response.data);
+            const backendRoot = import.meta.env.VITE_BACKEND_ROOT
+            const plotUrl = `${backendRoot}${response.data.plot_img}`
+            const ma100Url = `${backendRoot}${response.data.plot_100_dma}`
+            const ma200Url = `${backendRoot}${response.data.plot_200_dma}`
+            const predictionUrl = `${backendRoot}${response.data.plot_prediction}`
+            setPlot(plotUrl)
+            setMA100(ma100Url)
+            setMA200(ma200Url)
+            setPrediction(predictionUrl)
+            setMSE(response.data.mse)
+            setR2(response.data.r2)
+            setRMSE(response.data.rmse)
+            // Set Plots
+            if (response.data.error){
+                setError(response.data.error)
+            }
+        }catch(error){
+            console.error('There was an error making the API request', error)
+        } finally {
+            setLoading(false);
+        }
+    }
   return (
-    <div className='text-light container'>Dashboard</div>
+    <div className='container'>
+        <div className='row'>
+            <div className='col-md-6 mx-auto'>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" className='form-control' placeholder='Enter the stock ticker'
+                    onChange={(e) => setTicker(e.target.value)} required/>
+                    <small>{error && <div className='text-danger'>{error}</div>}</small>
+                    <button type='submit' className='btn btn-info mt-3'>
+                        {loading? <span><FontAwesomeIcon icon={faSpinner} spin />Please Wait ...</span>: 'See Prediction'}
+                    </button>
+                </form>
+            </div>
+            {/* Print Prediction plots */}
+            {prediction && (
+    <>
+        <div className='prediction mt-5'>
+            <div className='p-5'>
+                {plot && (
+                    <img src={plot} style={{ maxWidth: '100%' }} />
+                )}
+            </div>
+        </div>
+
+        <div className='p-3'>
+            {ma100 && <img src={ma100} style={{ maxWidth: '100%' }} />}
+        </div>
+
+        <div className='p-3'>
+            {ma200 && <img src={ma200} style={{ maxWidth: '100%' }} />}
+        </div>
+
+        <div className='p-3'>
+            {prediction && <img src={prediction} style={{ maxWidth: '100%' }} />}
+        </div>
+
+        <div className="text-light p-3">
+            <h4>Model Evaluation</h4>
+            <p>MSE: {mse}</p>
+            <p>RMSE: {rmse}</p>
+            <p>R²: {r2}</p>
+        </div>
+    </>
+)}
+        </div>
+    </div>
   )
 }
 
